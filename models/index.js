523aa36119e93c09f73a499789da44cbcaf75f1e
jberger2992 <jberger2992@gmail.com>
2023-05-30 14:15:24 -0400

    added files

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require("./User");
+const World = require("./World");
+const Adventure = require("./Adventure");
+const Info = require("./Info");
+
+
+module.exports = {
+    User,
+    World,
+    Adventure,
+    Info
+}
